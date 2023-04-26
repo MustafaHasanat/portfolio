b5260b81ca76acd8d7c0891c24397d928415fb1e
MustafaHasanat <mustafa.hasanat99@gmail.com>
2023-04-26 20:36:47 +0300

    setting the footer form

---
+++ utils/firebase/index.ts
@@ -0,0 +1,18 @@
+// import { initializeApp } from "firebase/app";
+// import { getMessaging } from "firebase/messaging";
+
+// var firebaseConfig = {
+//     apiKey: "AIzaSyCy48Ut_-3YVlnHHILZKAHcb4M4Sxx93fU",
+//     authDomain: "portfolio-2641999.firebaseapp.com",
+//     databaseURL: "https://DATABASE_NAME.firebaseio.com",
+//     projectId: "portfolio-2641999",
+//     storageBucket: "portfolio-2641999.appspot.com",
+//     messagingSenderId: "SENDER_ID",
+//     appId: "APP_ID",
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+// // Initialize Firebase Cloud Messaging and get a reference to the service
+// const messaging = getMessaging(app);
